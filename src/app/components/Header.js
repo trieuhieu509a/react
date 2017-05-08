dd41d8381a21df767659e0a65de27e9131573bde
Hieu Trinh <trieuhieu509a@gmail.com>
2017-05-08 18:16:12 +0700

    finished lession 13

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,18 +3,16 @@
  */
 import React from "react";
 
-export class Header extends React.Component {
-    render(){
-        return (
-            <nav className="navbar navbar-default">
-                <div className="container">
-                    <div className="navbar-header">
-                        <ul className="nav navbar-nav">
-                            <li><a href="#">Home</a></li>
-                        </ul>
-                    </div>
+export const Header = (props) => {
+    return (
+        <nav className="navbar navbar-default">
+            <div className="container">
+                <div className="navbar-header">
+                    <ul className="nav navbar-nav">
+                        <li><a href="#">{props.homeLink}</a></li>
+                    </ul>
                 </div>
-            </nav>
-        );
-    }
-}
+            </div>
+        </nav>
+    );
+};
--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -9,7 +9,8 @@ export class Home extends React.Component {
         super();
         this.state = {
             age : props.initialAge,
-            status : 0
+            status : 0,
+            homeLink : props.initialLinkName
         };
         setTimeout(()=>{
             this.setState({
@@ -24,6 +25,18 @@ export class Home extends React.Component {
         });
     }
 
+    onChangeLink(){
+        console.log(this.state);
+        this.props.changeLink(this.state.homeLink);
+    }
+
+    onHandleChange(event){
+        console.log(this.state);
+        this.setState({
+            homeLink: event.target.value
+        });
+    }
+
     render(){
         return (
             <div>
@@ -31,6 +44,14 @@ export class Home extends React.Component {
                 <p>Your name is {this.props.name}, you age is {this.state.age}</p>
                 <p>Status : {this.state.status}</p>
                 <button onClick={()=> this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
+                <hr/>
+                <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
+                <hr/>
+                <input type="text"
+                       value={this.state.homeLink}
+                       onChange={(event)=> this.onHandleChange(event)} />
+                <hr/>
+                <button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Change Header Link</button>
             </div>
         );
     }
@@ -38,7 +59,9 @@ export class Home extends React.Component {
 
 Home.propTypes = {
     name : React.PropTypes.string,
-    initialAge : React.PropTypes.number,/*
+    initialAge : React.PropTypes.number,
+    greet : React.PropTypes.func,
+    initialLinkName : React.PropTypes.string,/*
     user : React.PropTypes.object,
     children : React.PropTypes.element.isRequired*/
 };
